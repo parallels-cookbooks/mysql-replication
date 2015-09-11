6525b27999d115085b80b179bc0e79396c9be51c
Pavel Yudin <pyudin@parallels.com>
2015-09-11 16:00:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mysql-replication
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'mysql-replication'
+maintainer 'Pavel Yudin'
+maintainer_email 'pyudin@parallels.com'
+license 'apache2'
+description 'Installs/Configures mysql-replication'
+long_description 'Installs/Configures mysql-replication'
+version '0.1.0'
