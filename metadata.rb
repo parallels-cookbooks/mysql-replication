3b11fdbd565649de80027871265a823f314b532b
Pavel Yudin <pyudin@parallels.com>
2015-10-01 16:18:57 +0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license 'Apache 2.0'
 description 'Installs/Configures mysql-replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '1.0.0'
 
 supports 'amazon'
 supports 'redhat'
