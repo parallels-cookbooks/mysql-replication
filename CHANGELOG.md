5b5fea6072533ee6c52ea8f84feec2c9af15784d
Pavel Yudin <pyudin@parallels.com>
2017-03-31 18:24:23 +0300

    Bump version to v2.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Mysql-replication Cookbook Changelog
+
+## 2.0.0 (March 31, 2017)
+- Switch from HWRP to new Chef resources
+- Added a support of mysql cookbook newer then 8.0
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license 'Apache 2.0'
 description 'Installs/Configures mysql-replication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '2.0.0'
 issues_url 'https://github.com/parallels-cookbooks/mysql-replication/issues'
 source_url 'https://github.com/parallels-cookbooks/mysql-replication'
 
